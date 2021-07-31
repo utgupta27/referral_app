08a3d13abe98ffba78e35df4109658ec458f9720
utgupta27 <utgupta27@gmail.com>
2021-07-31 20:57:57 +0530

    debug banner removed

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Referral App",
+      debugShowCheckedModeBanner: false,
       home: SignInScreen(),
     );
   }
